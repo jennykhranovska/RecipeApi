a060293bcbb404a354fd35f956e82e3c58bf6798
balac <balachomar@gmail.com>
2026-03-24 15:59:35 +0100

    Lagt till RecipeService och Interface

--- RecipeApi/Services/IRecipeService.cs
+++ RecipeApi/Services/IRecipeService.cs
@@ -1,6 +1,14 @@
-﻿namespace RecipeApi.Services
+﻿using RecipeApi.Models;
+
+namespace RecipeApi.Services
 {
-    public class IRecipeService
+    public interface IRecipeService
     {
+        Task<IEnumerable<Recipe>> GetAllRecipesAsync();
+        Task<Recipe?> GetRecipeByIdAsync(int id);
+        Task<Recipe> CreateRecipeAsync(Recipe recipe);
+        Task UpdateRecipeAsync(Recipe recipe);
+        Task DeleteRecipeAsync(int id);
+        Task<IEnumerable<Recipe>> SearchRecipesAsync(string term);
     }
 }
--- RecipeApi/Services/RecipeService.cs
+++ RecipeApi/Services/RecipeService.cs
@@ -1,6 +1,50 @@
-﻿namespace RecipeApi.Services
+﻿using RecipeApi.Models;
+using RecipeApi.Repositories;
+
+namespace RecipeApi.Services
 {
-    public class RecipeService
+    public class RecipeService : IRecipeService
     {
+        private readonly IRecipeRepository _repository;
+
+        public RecipeService(IRecipeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Recipe>> GetAllRecipesAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Recipe?> GetRecipeByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<Recipe> CreateRecipeAsync(Recipe recipe)
+        {
+            return await _repository.CreateAsync(recipe);
+        }
+
+        public async Task UpdateRecipeAsync(Recipe recipe)
+        {
+            await _repository.UpdateAsync(recipe);
+        }
+
+        public async Task DeleteRecipeAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Recipe>> SearchRecipesAsync(string term)
+        {
+            if (string.IsNullOrWhiteSpace(term))
+            {
+                return await _repository.GetAllAsync();
+            }
+
+            return await _repository.SearchAsync(term);
+        }
     }
 }
